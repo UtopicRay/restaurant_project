6d6bc4cc0b890de53df5f15ea20d70ed3bcc39f6
UtopicRay <rayrm2021@gmail.com>
2024-07-19 17:11:29 -0400

    'Add useGetData hook'

---
+++ src/hook/useGetData.ts
@@ -0,0 +1,18 @@
+import { useState } from "preact/hooks";
+
+export function useGetData() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
+
+  async function getRecipes({ name, limit }: { name: string; limit: string }) {
+    await fetch(
+      `https://dummyjson.com/recipes?limit=${limit ? limit : "10"}${
+        name ? `&select=${name}` : ""
+      },image`
+    )
+      .then((response) => response.json())
+      .then((data) => setData(data)).finally(()=>setIsLoading(false)).catch((error:Error)=>setError(error.message()));
+  }
+  return { data,isLoading,error,getRecipes };
+}
