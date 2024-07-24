953206b147f177d21ff0f9d1ba5dd219751f047b
UtopicRay <rayrm2021@gmail.com>
2024-07-24 15:07:40 -0400

    'Add getRecipeById'

--- src/services/recipes.ts
+++ src/services/recipes.ts
@@ -1,4 +1,4 @@
-import type { RecipesAPI } from "../type/recipesAPI";
+import type { Recipe, RecipesAPI } from "../type/recipesAPI";
 
 export async function getRecipes(name:string,limit:string) {
   const response = await fetch(
@@ -6,4 +6,9 @@ export async function getRecipes(name:string,limit:string) {
   );
   const {recipes} = (await response.json()) as RecipesAPI;
   return recipes;
+}
+export async function getRecipeById(id:string) {
+  const response=await fetch(`https://dummyjson.com/recipes/${id}`)
+  const recipe=await response.json() as Recipe
+  return recipe;
 }
