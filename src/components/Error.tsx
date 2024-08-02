ef35a0695db228cbea0c018c7c2d545985219760
UtopicRay <ray2021@gmail.com>
2024-08-02 01:55:12 -0400

    'Create Error Component'

---
+++ src/components/Error.tsx
@@ -0,0 +1,12 @@
+
+function Error() {
+    return (
+        <div className='w-full flex justify-center items-center'>
+            <h1 className='mt-2 font-bold text-2xl text-white'>
+                Algo ha salido mal. Por favor vuelva a intentarlo.
+            </h1>
+        </div>
+    );
+}
+
+export default Error;
