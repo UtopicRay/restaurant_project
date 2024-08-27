a6d115f68c4c2235e9f0f7eeab64dffb612790bc
UtopicRay <ray2021@gmail.com>
2024-08-27 13:11:27 -0400

    'Install sharp dependency'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), preact({ compat: true })],
-    output:"hybrid"
+    output:"hybrid",
+    image: {
+        service: passthroughImageService(),
+    },
 });
