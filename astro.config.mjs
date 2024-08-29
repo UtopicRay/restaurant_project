39ed37b8a251c84eae7f30c01684889aebd3a487
UtopicRay <ray2021@gmail.com>
2024-08-29 15:26:17 -0400

    'Add netlify adapter'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
-import { defineConfig, passthroughImageService} from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), preact({ compat: true })],
-    output:"hybrid",
-    image: {
-        service: passthroughImageService(),
-    },
+  integrations: [tailwind(), preact({
+    compat: true
+  })],
+  output: "hybrid",
+  image: {
+    service: passthroughImageService()
+  },
+  adapter: netlify()
 });
